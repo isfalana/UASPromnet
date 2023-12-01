df349cc99824e41bca1751cf497f3c6e4f3f8e82
isfalana <isfalana13@upi.edu>
2023-12-01 19:44:11 +0700

    update .

---
+++ main.js
@@ -0,0 +1,15 @@
+// const div1Element = document.querySelector(div.div1);
+
+// const div1Observer = new IntersectionObserver(
+//   (entries) => {
+//     const [entry] = entries;
+
+//     if (!entry.isIntersecting) {
+//       document.querySelector("nav").classList.add("scrolled");
+//       console.log("p");
+//     }
+//   },
+//   { threshold: 0.9 }
+// );
+
+// div1Observer.observe(div1Element);
